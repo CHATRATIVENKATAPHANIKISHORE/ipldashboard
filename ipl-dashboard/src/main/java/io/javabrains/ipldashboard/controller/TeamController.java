c692bbfc34452be39bac242c18dfbe912e780593
kishore <890738@cognizant.com>
2021-07-11 21:00:20 +0530

    Add all my files

---
+++ ipl-dashboard/src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
@@ -0,0 +1,63 @@
+package io.javabrains.ipldashboard.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.javabrains.ipldashboard.model.Match;
+import io.javabrains.ipldashboard.model.Team;
+import io.javabrains.ipldashboard.repository.MatchRepository;
+import io.javabrains.ipldashboard.repository.TeamRepository;
+
+@RestController
+@CrossOrigin
+public class TeamController {
+
+	
+	private TeamRepository teamRepository;
+	private MatchRepository matchRepository;
+	
+	
+public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) {
+		super();
+		this.teamRepository = teamRepository;
+		this.matchRepository=matchRepository;
+	}
+	
+	@GetMapping("/team")
+	public Iterable<Team> getAllTeam() {
+		return this.teamRepository.findAll();
+		//Pageable pageable = PageRequest.of(0,4);
+		
+		
+		
+		
+	}
+	
+	@GetMapping("/team/{teamName}")
+	public Team getTeam(@PathVariable String teamName) {
+		Team team =  this.teamRepository.findByTeamName(teamName);
+		//Pageable pageable = PageRequest.of(0,4);
+		
+		team.setMatches(matchRepository.findLatestMatchesbyTeam(teamName, 4));
+		return team;
+		
+		
+	}
+	@GetMapping("/team/{teamName}/matches")
+	public List<Match> getMachesForTeam(@PathVariable String teamName, @RequestParam int year){
+		LocalDate startDate = LocalDate.of(year, 1, 1);
+		LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+		return this.matchRepository.getMatchByBetweenDates(teamName, startDate, endDate);
+				//getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(teamName, endDate, endDate, teamName, startDate, endDate); 
+		
+				
+	}
+}
